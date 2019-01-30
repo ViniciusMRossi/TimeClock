e19027bff11120e9be9226857ee2b4e9ee1c6a19
Vinicius Rossi <vinimrossi@gmail.com>
2019-01-30 21:02:24 -0200

    Create first module: login

--- TimeClock/app/src/main/java/com/viniciusrossi/timeclock/MainActivity.kt
+++ TimeClock/app/src/main/java/com/viniciusrossi/timeclock/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.viniciusrossi.timeclock
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.viniciusrossi.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
